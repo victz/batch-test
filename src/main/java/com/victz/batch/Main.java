11fc55cf5ebc08bd5aa0f8ce184350f9393641db
Shawn Victz <shawn@victz.com>
2023-07-24 23:45:41 +0800

    Initial commit.

---
+++ src/main/java/com/victz/batch/Main.java
@@ -0,0 +1,12 @@
+package com.victz.batch;
+
+/**
+ *
+ * @author shawn
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
